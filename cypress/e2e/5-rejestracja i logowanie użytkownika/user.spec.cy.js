722fd5396239be96216904a60cdca22a0abbe9a4
martularozmus <martularozmus@gmail.com>
2024-07-23 14:58:11 +0200

    add rejestracja i logowanie

---
+++ cypress/e2e/5-rejestracja i logowanie użytkownika/user.spec.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Rejestracja i logowanie użytkownika na Sephora', () => {
+    it('Powinno rejestrować nowego użytkownika', () => {
+        cy.visit('https://www.sephora.pl/register');
+        cy.get('input[name="firstName"]').type('Jan');
+        cy.get('input[name="lastName"]').type('Kowalski');
+        cy.get('input[name="email"]').type('jan.kowalski@example.com');
+        cy.get('input[name="password"]').type('Test1234!');
+        cy.get('button[type="submit"]').click();
+        cy.get('.account-dashboard').should('be.visible');
+    });
+
+    it('Powinno logować użytkownika', () => {
+        cy.visit('https://www.sephora.pl/login');
+        cy.get('input[name="email"]').type('jan.kowalski@example.com');
+        cy.get('input[name="password"]').type('Test1234!');
+        cy.get('button[type="submit"]').click();
+        cy.get('.account-dashboard').should('be.visible');
+    });
+});
